2db6f48426142d3beb57d6486c52d6daf6836566
VALENTINJUKOV99 <macbook@192.168.1.134>
2025-10-20 15:40:25 +0300

    restore src/App.tsx and vite.config.ts from backup commit a6a313b

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-```typescript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.includes('node_modules')) {
-            if (id.includes('firebase')) return 'firebase-vendor';
-            if (id.includes('react')) return 'react-vendor';
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
 })
-
-```
